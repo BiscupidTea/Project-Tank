b15d4b4cfb1ce7adde3802ec2ce03117ba0ac5d6
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-04-17 09:14:38 -0300

    add turret

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -20,7 +20,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float turnSpeed = 0f;
 
     private float HorizontalMovement;
+    private bool IsRotating;
     private float VerticalMovement;
+    private bool IsMoving;
 
     private Vector3 initialFoward;
 
@@ -29,14 +31,40 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         _rigidbody ??= GetComponent<Rigidbody>();
+
         initialFoward = transform.forward;
+
         turnSpeed = MaxTurnSpeed;
+        movementSpeed = MaxMovementSpeed;
     }
     private void FixedUpdate()
     {
+        ModifyTrayectory(movementSpeed);
+
+        ModifyTurnRotation();
+
         _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(Vector3.up * HorizontalMovement * turnSpeed * Time.deltaTime));
 
+        ModifyMovementSpeed();
+
         _rigidbody.velocity = transform.forward * VerticalMovement * movementSpeed + Vector3.up * _rigidbody.velocity.y;
+    }
+
+    private void ModifyMovementSpeed()
+    {
+        movementSpeed = IsRotating ? MaxMovementSpeed / 2 : MaxMovementSpeed;
+    }
+
+    private void ModifyTurnRotation()
+    {
+        if (IsMoving)
+        {
+            turnSpeed = MaxTurnSpeed;
+        }
+        else
+        {
+            turnSpeed = MaxTurnSpeed + MaxTurnSpeed / 8;
+        }
     }
 
     public void OnMoveFB(InputValue input)
@@ -44,7 +72,10 @@ public class PlayerMovement : MonoBehaviour
         var currentInput = input.Get<float>();
 
         VerticalMovement = currentInput;
+    }
 
+    private void ModifyTrayectory(float currentInput)
+    {
         if (currentInput > 0)
         {
             _currentMovement = transform.forward;
@@ -58,10 +89,21 @@ public class PlayerMovement : MonoBehaviour
             _currentMovement = Vector3.zero;
         }
 
+        IsMoving = VerticalMovement != 0 ? true : false;
     }
 
     public void OnMoveRo(InputValue input)
     {
         HorizontalMovement = input.Get<float>();
+
+        if (HorizontalMovement != 0)
+        {
+            IsRotating = true;
+        }
+        else
+        {
+            IsRotating = false;
+        }
+
     }
 }
